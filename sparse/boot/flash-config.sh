c43191e9a643736e2b25eb27b975635ec51d8de5
Matti Lehtimaki <matti.lehtimaki@gmail.com>
2024-12-06 14:23:55 +0200

    configs: Initial version for Fairphone 4
    [configs] Initial version for Fairphone 4

---
+++ sparse/boot/flash-config.sh
@@ -0,0 +1,19 @@
+VALID_PRODUCTS=(FP4)
+
+FLASH_OPS=(
+"getvar_fail_if unlocked no"
+"flash boot_a hybris-boot.img"
+"flash boot_b hybris-boot.img"
+"flash dtbo_a dtbo.img"
+"flash dtbo_b dtbo.img"
+"flash userdata sailfish.img001"
+)
+
+GETVAR_ERROR_unlocked="
+This device has not been unlocked, but you need that for flashing.
+Please go to https://www.fairphone.com/en/bootloader-unlocking-code-for-fairphone/ and see instructions how to unlock your device.
+"
+
+FLASH_COMPLETED_MESSAGE="
+Remove the USB cable and bootup the device by pressing powerkey.
+"
